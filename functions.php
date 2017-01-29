9c02d76163f213a45a89ab06db5a5244304d439c
scrabill <shannon.crabill@gmail.com>
2017-01-28 23:06:46 -0500

    enqueue styles

--- functions.php
+++ functions.php
@@ -1,7 +1,14 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
+    $parent_style = 'parent-style'; // This is 'twentyseventeen-style' for the Twenty twentyseventeen theme.
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
 }
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 ?>
