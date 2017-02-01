624c8974f4325192c2c923de2400577809ef9781
scrabill <shannon.crabill@gmail.com>
2017-01-31 22:18:09 -0500

    fix child theme chaching issues

--- functions.php
+++ functions.php
@@ -1,8 +1,6 @@
 <?php
 function my_theme_enqueue_styles() {
-
     $parent_style = 'parent-style'; // This is 'twentyseventeen-style' for the Twenty twentyseventeen theme.
-
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
